d35e7767d68941c63c8eef16e0631bac0a81fe57
Fernando Rueda <fernandorueda@Fernandos-Mac-Pro.local>
2020-03-05 00:03:25 -0600

    initial commit

---
+++ src/main/java/com/curiouscatsoftware/queryExtractor/App.java
@@ -0,0 +1,13 @@
+package com.curiouscatsoftware.queryExtractor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
